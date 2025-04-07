3499f2bba2b143fa369f45f3726bff9569c355d4
manmeetmann667 <mann2018meet@gmail.com>
2025-04-07 22:55:24 +0530

    js files

---
+++ script.js
@@ -0,0 +1,63 @@
+// console.log("Hello World")
+
+// var a  = 5
+// var b = 10
+
+// console.log(a+b)
+
+// function name(name){
+//     console.log("This is name" + name)
+// }
+
+// name("manmeet")
+
+// const function1 = (x)=>{
+//     console.log(x)
+// }
+// function1(34)
+
+// let num1  = parseFloat(prompt("enter first number:"));
+// let num2  = parseFloat(prompt("enter second number:"));
+// let operator  = (prompt("enter operation:"));
+
+// function calculator(a,b,operation){
+
+//     let isfault = Math.random()<0.1;
+
+//     if (isfault){
+//         switch(operation) {
+//             case '+':
+//               return a - b; 
+//             case '-':
+//               return a / b; 
+//             case '*':
+//               return a + b; 
+//             case '/':
+//               return a ** b; 
+//             default:
+//               return "Invalid operator";
+//           }
+
+//     }
+//     else{
+//         switch(operation) {
+//             case '+':
+//               return a + b;
+//             case '-':
+//               return a - b;
+//             case '*':
+//               return a * b;
+//             case '/':
+//               return a / b;
+//             default:
+//               return "Invalid operator";
+//           }
+//     }
+
+// }
+
+// calculator(num1,num2,operator)
+
+ a = "manmeet";
+ console.log(a.concat("Manmeet"));
+ console.log(a);
